bf5e5e596c211684293da0817adb87f7e956cb5d
lofeodo <daniel.lofeodo@gmail.com>
2025-12-31 17:41:51 -0500

    remove superfluous floating add button in the create workout screen

--- lib/screens/create_workout_screen.dart
+++ lib/screens/create_workout_screen.dart
@@ -136,10 +136,6 @@ class _CreateWorkoutScreenState extends ConsumerState<CreateWorkoutScreen>
             }),
         ],
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _addExerciseDialog,
-        child: const Icon(Icons.add),
-      ),
     );
   }
 }
